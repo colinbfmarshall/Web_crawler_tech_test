c72f33eb7135e823b439f1d1f28e567693ebb9f7
colinbfmarshall <colinbfmarshall@gmail.com>
2016-12-07 07:57:13 +0000

    have all the links from the page

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,17 +1,28 @@
 class Query < ApplicationRecord
   require 'open-uri'
+  require 'open_uri_redirections'
   
   before_save :check_url
   after_save :lets_do_some_scrapping
 
   def lets_do_some_scrapping
+    #use nokogiri to get all href (a) tags from page
     page = Nokogiri::HTML(open(self.url))
     
-    links = page.css("a")
+    #go through list of a tags and remove any duplicates with uniq!
+    links = page.css("a").map { | link| link['href'] }.uniq!
+    
+    #remove any a tags that don't contain http or https
+    links.select! { |link| link[/\Ahttp:\/\//] || link[/\Ahttps:\/\//] }
 
-    raise links.count.inspect
+    clean_links = links.map do |link|
+      Nokogiri::HTML(open(link, :allow_redirections => :all))
+    end
+
+    raise clean_links.inspect
 
   end
+
 
 protected
 
